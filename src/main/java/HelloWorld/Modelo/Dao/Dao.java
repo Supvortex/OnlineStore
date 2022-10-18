776a9647775794dd8b93a94d9817a05209dda058
Jose Lopez <supvortex@gmail.com>
2022-10-18 23:22:03 +0200

    JoseLopez: add DAO functions and IDAO

--- src/main/java/HelloWorld/Modelo/Dao/Dao.java
+++ src/main/java/HelloWorld/Modelo/Dao/Dao.java
@@ -1,8 +1,9 @@
 package HelloWorld.Modelo.Dao;
-
 import HelloWorld.Modelo.Articulo;
 import HelloWorld.Modelo.Cliente;
 import HelloWorld.Modelo.Pedido;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,52 +18,78 @@ public class Dao {
         this.clientes = new ArrayList<Cliente>();
         this.articulos = new ArrayList<Articulo>();
     }
-    void anadirPedido(Pedido pedido){
-        //TODO
+
+    void anadirPedido(Pedido pedido) {
+        this.pedidos.add(pedido);
     }
-    void cancelarPedido(Pedido numPedido){
-        //TODO
+
+    void cancelarPedido(Pedido pedido) {
+        if (esCancelable(pedido) == true) {
+            this.pedidos.remove(pedido);
+        }
     }
-    List<Pedido> mostrarPedidos (Cliente cliente, Boolean enviado ){
-        List<Pedido> pedidos = new ArrayList<Pedido>();
-        //TODO
-        return pedidos;
+
+    List<Pedido> mostrarPedidos() {
+        return this.pedidos;
     }
-    void anadirCliente (Cliente cliente){
-        //TODO
+
+    void anadirCliente(Cliente cliente) {
+        if (!clienteExiste(cliente)) {
+            this.clientes.add(cliente);
+        }
     }
-    void clienteExiste (Cliente cliente){
-        //TODO
+
+    Boolean clienteExiste(Cliente cliente) {
+        for (Cliente myCliente : this.clientes) {
+            if (cliente.getEmail().equals(myCliente.getEmail())) {
+                return true;
+            }
+        }
+        return false;
     }
-    List<Cliente> mostrarClientes (){
-        List<Cliente> clientes = new ArrayList<Cliente>();
-        //TODO
-        return clientes;
+
+    List<Cliente> mostrarClientes() {
+        return this.clientes;
     }
-    List<Cliente> mostrarClientesPrem (Boolean esPremium ){
+
+    List<Cliente> mostrarClientesPrem() {
         List<Cliente> clientesPrem = new ArrayList<Cliente>();
-        //TODO
+        for (Cliente myCliente : this.clientes) {
+            if (myCliente.getPrem()) {
+                clientesPrem.add(myCliente);
+            }
+        }
         return clientesPrem;
     }
-    void anadirArticulo(Articulo articulo){
-        //TODO
+
+    void anadirArticulo(Articulo articulo) {
+        if (!hayArticulo(articulo)) {
+            this.articulos.add(articulo);
+        }
     }
-    List<Articulo> mostrarArticulos(){
-        List<Articulo> articulos = new ArrayList<Articulo>();
-        //TODO
-        return articulos;
+
+    List<Articulo> mostrarArticulos() {
+        return this.articulos;
     }
-    private Boolean esCancelable(Pedido pedido){
-        Boolean cancelable = false;
-        //TODO Nota- si es cancelable, es que no está enviado
-        return cancelable;
+
+    private Boolean esCancelable(Pedido pedido) {
+        if (Duration.between(pedido.getFechaHora().toLocalDate(), LocalDateTime.now().toLocalDate()).getSeconds() > (pedido.getArticulo().getTiempoEnvio() * 60)) {
+            return false;
+        } else {
+            return true;
+        }
     }
-    private Boolean estaEnviado(Pedido pedido){
+
+    private Boolean estaEnviado(Pedido pedido) {
         return !esCancelable(pedido);
     }
-    private Boolean hayArticulo(Articulo articulo){
-        Boolean existeArticulo = false;
-        //TODO
-        return existeArticulo;
+
+    private Boolean hayArticulo(Articulo articulo) {
+        for (Articulo myArticulo : this.articulos) {
+            if (articulo.getCodigo().equals(myArticulo.getCodigo())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
